035d25dd352ab98583d23fe3d214126bc3f6775a
jacobtt21 <jacobt1206@gmail.com>
2024-08-09 13:10:58 -0500

    fixed small bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,11 +7,11 @@ export function cn(...inputs: ClassValue[]) {
 
 export function createGradient() {
   const gradients = [
-    "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);",
-    "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%);",
-    "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%);",
-    "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);",
-    "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%);",
+    "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
+    "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)",
+    "linear-gradient(62deg, #8EC5FC 0%, #E0C3FC 100%)",
+    "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%)",
+    "linear-gradient(0deg, #08AEEA 0%, #2AF598 100%)",
   ];
 
   return gradients[Math.floor(Math.random() * gradients.length)];
