2a0dc50c5f052e3e93d23d15bf981dbc462058f5
jacobtt21 <jacobt1206@gmail.com>
2024-09-21 12:46:34 -0500

    added vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import localFont from 'next/font/local'
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Startblock | The delightfully simple ATS",
@@ -40,6 +41,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`font-text ${heading.variable} ${text.variable} ${accent.variable} ${special.variable}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
