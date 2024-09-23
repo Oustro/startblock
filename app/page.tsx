738abddfa67638bcabd976b1db07f164a92a8e52
jacobtt21 <jacobt1206@gmail.com>
2024-09-23 09:37:32 -0500

    added cal

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import LogoNav from "@/components/spc/navbars/logoNav";
 import ActionButton from "@/components/ui/action-button";
+import ActionWord from "@/components/ui/action-word";
 import Footer from "@/components/ui/footer";
 
 import Link from "next/link";
@@ -15,6 +16,11 @@ export default async function Home() {
           Sign in
         </ActionButton>
       </Link>
+      <Link target="_blank" href="https://cal.com/ziggy/15min">
+          <ActionWord className="w-30 mt-8 ml-4">
+            Schedule a demo
+          </ActionWord>
+        </Link>
       <p className="mt-8 text-our-gray text-sm font-highlight">Application for</p>
       <h1 className='font-heading text-3xl'>StartBlock Partner</h1>
       <p className="mt-5 text-our-gray text-sm font-highlight">Location</p>
